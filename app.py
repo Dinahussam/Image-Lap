d98c1c73ffa1d58600f1f3e220c502f17a2476e6
Sama Mostafa <SamaMostafa1>
2022-12-28 22:42:34 +0200

    cropper.js

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 
 from flask import Flask, jsonify, render_template, request
-
+import os
+import sys
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = 'upload/'
 app = Flask(__name__, template_folder="templates")
-
-
-@app.route('/')
-def home():
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+@app.route('/', methods=['GET'])
+def index():
     return render_template('main.html')
+
+# @app.route('/image', methods=['POST'])
+# def image():
+#     if request.files['file']:
+#         # File Saving
+#         file = request.files['file']
+#         #abspath = os.path.dirname(__file__)
+#         file_path = os.path.join(
+#             app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+#         file.save(file_path)
+#         print( file_path)
+        
 if __name__ == '__main__':
     app.run(debug=True) 
--- static/js/main.js
+++ static/js/main.js
@@ -2,168 +2,58 @@ let magnitudeImageInput = document.querySelector("#magnitudeImageInput");
 let phaseImageInput = document.querySelector("#phaseImageInput");
 let magnitudeImageBtn = document.querySelector(".magnitudeImageBtn");
 let phaseImageBtn = document.querySelector(".phaseImageBtn");
-let rectArray = [];
-let magPoints = [];
-let phasePoints = [];
+let magnitudeImage = document.querySelector("#magnitudeImage");
+let phaseImage = document.querySelector("#phaseImage");
+let magFlag=0;
+let phaseFlag=0;
 var path = "";
-let isNowDrawing = false;
-function drawStage(contain){
-var stage = new Konva.Stage({
-  container: contain,
-  width: 800,
-  height: 300
-});
-return stage;
-}
-function drawLayer(stage){
-var layer = new Konva.Layer();
-stage.add(layer);
-stage.draw();
-return layer;
-}
-function zoom(stage){
-  
-  var scaleBy = 1.01;
-  stage.on('wheel', (e) => {
-    // stop default scrolling
-    e.evt.preventDefault();
-
-    var oldScale = stage.scaleX();
-    var pointer = stage.getPointerPosition();
-
-    var mousePointTo = {
-      x: (pointer.x - stage.x()) / oldScale,
-      y: (pointer.y - stage.y()) / oldScale,
-    };
-
-    // how to scale? Zoom in? Or zoom out?
-    let direction = e.evt.deltaY > 0 ? 1 : -1;
-
-    // when we zoom on trackpad, e.evt.ctrlKey is true
-    // in that case lets revert direction
-    if (e.evt.ctrlKey) {
-      direction = -direction;
-    }
-
-    var newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
-
-    stage.scale({ x: newScale, y: newScale });
-
-    var newPos = {
-      x: pointer.x - mousePointTo.x * newScale,
-      y: pointer.y - mousePointTo.y * newScale,
-    };
-    stage.position(newPos);
-});
-}
-function rectDown(stage,layer){
-  rect = new Konva.Rect({
-    x: stage.getPointerPosition().x,
-    y: stage.getPointerPosition().y,
-    width: 0,
-    height: 0,
-    fill: "transparent",
-    stroke: "#14d4ed",
-    strokeWidth: 2,
+function getIndex(image,path){
+  image.src = `${path}`;
+  cropper = new Cropper( image, {
+  });	
+  image.addEventListener('crop', (event) => {
+  cropper.getCroppedCanvas().toBlob(function (blob){
+    console.log(blob);
+    var data=cropper.getData();
+    var indexs=Object.values(data);
+    console.log(data);
+    console.log(indexs);
+    var formData = new FormData();
+    formData.append('croppedImage', blob);
+    console.log(formData);
+    // $.ajax({
+    //   type: "POST",
+    //   url: "/image",
+    //   data: formData,
+    //   contentType: false,
+    //   cache: false,
+    //   processData: false,
+    //   async: true,
+    // });
   });
-  layer.add(rect);
-  layer.draw();
-}
-function rectMove(stage){
-  const newWidth = stage.getPointerPosition().x - rect.x();
-  const newHeight = stage.getPointerPosition().y - rect.y();
-  rect.width(newWidth);
-  rect.height(newHeight);
-  return stage.getPointerPosition().x,stage.getPointerPosition().y
-}
-function drawRect(stage,layer){
-  rectArray=[];
-  stage.on("mousedown ", mousedownHandler);
-  stage.on("mousemove ", mousemoveHandler);
-  stage.on("mouseup ", mouseupHandler);
-  function mousedownHandler() {
-      // console.log(stage.getPointerPosition().x);
-      // console.log(stage.getPointerPosition().y);
-      rectArray.push(stage.getPointerPosition().x);
-      rectArray.push(stage.getPointerPosition().y);
-      isNowDrawing = true;
-      
-      rectDown(stage,layer);
-  }
-  function mousemoveHandler() {
-      
-      if (!isNowDrawing) return false;
-      rectMove(stage);
-  }
-  function mouseupHandler() {
-    
-      isNowDrawing = false;
-  }
-  
-  // rectArray.push(stage.getPointerPosition().y);
-  stage.add(layer);
-  return rectArray;
-}
-function drawImage(img,path,layer,stage){
-  img.src = `${path}`;
-  img.onload = function() {
-  var theImg = new Konva.Image({
-    image: img,
-    x: 0,
-    y: 0,
-    width: 800,
-    height: 300,
   });
-  layer.add(theImg);
-  layer.draw();
-  zoom(stage);
+ 
 }
+function upload_image_action(image,button) {
+  button.style.display = `none`;
 }
 magnitudeImageInput.addEventListener("change", function () { 
   let reader = new FileReader();
+  magnitudeImage.style.display = `flex`;
   reader.addEventListener("load" ,() => {
   path = reader.result;
-  var magnitudeImage = new Image();  
-  stageMagnitude=drawStage('canvas-magnitude');
-  layerMagnitude=drawLayer(stageMagnitude);
-  drawImage(magnitudeImage,path,layerMagnitude,stageMagnitude);
-  magPoints=drawRect(stageMagnitude,layerMagnitude);
-  console.log(magPoints);
-     // var formData = new FormData();
-    //   $.ajax({
-    //     type: "POST",
-    //     url: "/predict",
-    //     data: formData,
-    //     contentType: false,
-    //     cache: false,
-    //     processData: false,
-    //     async: true,
-    //   });
-   // });
+  getIndex(magnitudeImage,path);
+  upload_image_action(magnitudeImage,magnitudeImageBtn);
   });
   reader.readAsDataURL(this.files[0]);
 });
 phaseImageInput.addEventListener("change", function () {
   let reader = new FileReader();
+  phaseImage.style.display = `flex`;
   reader.addEventListener("load" ,() => {
   path = reader.result;
-  var phaseImage = new Image();  
-  stagePhase=drawStage('canvas-phase');
-  layerPhase=drawLayer(stagePhase);
-  drawImage(phaseImage,path,layerPhase,stagePhase);
-  points=drawRect(stagePhase,layerPhase);
-  console.log(phasePoints);
-      // var formData = new FormData();
-    //   $.ajax({
-    //     type: "POST",
-    //     url: "/predict",
-    //     data: formData,
-    //     contentType: false,
-    //     cache: false,
-    //     processData: false,
-    //     async: true,
-    //   });
-   // });
+  getIndex(phaseImage,path);
+  upload_image_action(phaseImage,magnitudeImageBtn);
   });
 reader.readAsDataURL(this.files[0]);
 });
@@ -180,4 +70,3 @@ reader.readAsDataURL(this.files[0]);
 //         document.getElementById("phaseImage-result").src = URL.createObjectURL(new Blob([byteArray], { type: 'image/png' }));
 //     }
 // }
-
