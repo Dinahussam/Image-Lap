97d1e24dcc2474954f46e418e7ecdf93b5bda1b8
Sama Mostafa <SamaMostafa1>
2022-12-30 19:32:46 +0200

    finishing konva with sending from front to back

--- app.py
+++ app.py
@@ -12,31 +12,28 @@ app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 def index():
     return render_template('main.html')
 
-@app.route('/image/<int:id>', methods=['POST'])
-def image(id):
-    if request.files['file']:
-        if id==1:
-        # File Saving
-            file_mag = request.files['file']
-            filename=secure_filename( file_mag .filename)
-            file_path_mag = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file_mag .save(file_path_mag)
-        else:
-            file_phase = request.files['file']
-            filename=secure_filename( file_phase .filename)
-            file_path_phase= os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file_phase .save(file_path_phase)
-    return []
-@app.route('/data/', methods=['POST'])
-# def data():
+# @app.route('/image/<int:id>', methods=['POST'])
+# def image(id):
+#     if request.files['file']:
+#         if id==1:
+#         # File Saving
+#             file_mag = request.files['file']
+#             filename=secure_filename( file_mag .filename)
+#             file_path_mag = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             file_mag .save(file_path_mag)
+#         else:
+#             file_phase = request.files['file']
+#             filename=secure_filename( file_phase .filename)
+#             file_path_phase= os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             file_phase .save(file_path_phase)
+#     return []
+# @app.route('/data/<int:id>', methods=['POST'])
+# def data(id):
 #     if request.method == 'POST':
+#             print(id)
 #             form=request.get_json()
 #             values = form['values']
-#             print(values)
-        
-            
+#             print(values)       
 #     return []
-
-    
 if __name__ == '__main__':
     app.run(debug=True) 
--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,111 @@
 let magnitudeImageInput = document.querySelector("#magnitudeImageInput");
 let phaseImageInput = document.querySelector("#phaseImageInput");
+let magnitudeImage = document.querySelector("#magnitudeImage");
 let magnitudeImageBtn = document.querySelector(".magnitudeImageBtn");
 let phaseImageBtn = document.querySelector(".phaseImageBtn");
-let magnitudeImage = document.querySelector("#magnitudeImage");
 let phaseImage = document.querySelector("#phaseImage");
+// let imageCrop=document.getElementById('image-box');
 let mag_icon = document.querySelector("#mag-icon");
 let phase_icon = document.querySelector("#phase-icon");
-let magFlag = 0;
+let rectFlag = 0;
 let phaseFlag = 0;
+let magFlag=0;
 var path = "";
-function getIndex(cropped, image) {
-  image.addEventListener("cropend", (event) => {
-    var data = cropped.getData();
-    values = Object.values(data);
-    console.log(values);
-    //   if(image===magnitudeImage){
-    //   $.ajax({
+let rect;
+let rectArray = [];
+let magPoints = [];
+let phasePoints = [];
+let isNowDrawing = false;
+var path = "";
+function upload_image_action(image,button) {
+  image.style.display = `flex`;
+  button.style.display = `none`;
+}
+function drawStage(contain){
+var stage = new Konva.Stage({
+  container: contain,
+  width: 500,
+  height: 320
+});
+return stage;
+}
+function drawLayer(stage){
+var layer = new Konva.Layer();
+stage.add(layer);
+stage.draw();
+return layer;
+}
+function circleDown(stage,layer) {
+  cir = new Konva.Circle({
+    x: stage.getPointerPosition().x,
+    y: stage.getPointerPosition().y,
+    radius: 0,
+    fill: "transparent",
+    stroke: "##1d27b6",
+    strokeWidth: 4,
+  });
+  layer.add(cir);
+  layer.draw();
+}
+function circleMove(stage) {
+  const rise = Math.pow(stage.getPointerPosition().y - cir.y(), 2);
+  const run = Math.pow(stage.getPointerPosition().x - cir.x(), 2);
+  const newRadius = Math.sqrt(rise + run);
+  cir.radius(newRadius);
+}
+
+function rectDown(stage,layer){
+  rect = new Konva.Rect({
+    x: stage.getPointerPosition().x,
+    y: stage.getPointerPosition().y,
+    width: 0,
+    height: 0,
+    fill: "transparent",
+    stroke: "#1d27b6",
+    strokeWidth: 2,
+  });
+  layer.add(rect);
+  rectArray.push(rect);
+  layer.draw();
+}
+function rectMove(stage){
+  const newWidth = stage.getPointerPosition().x - rect.x();
+  const newHeight = stage.getPointerPosition().y - rect.y();
+  rect.width(newWidth);
+  rect.height(newHeight);
+}
+function drawRect(stage,layer){
+  stage.on("mousedown ", mousedownHandler);
+  stage.on("mousemove ", mousemoveHandler);
+  stage.on("mouseup ", mouseupHandler);
+  values=[];
+  function mousedownHandler() {
+    if(rectArray.length>0){
+      rect.destroy();
+      values=[];
+    }
+      isNowDrawing = true;
+      values.push(stage.getPointerPosition().x);
+      values.push(stage.getPointerPosition().y);
+      rectDown(stage,layer);   
+  }
+  function mousemoveHandler() {
+
+      if (!isNowDrawing) return false;
+      rectMove(stage);
+  }
+  function mouseupHandler() {
+      isNowDrawing = false;
+      values.push(stage.getPointerPosition().x);
+      values.push(stage.getPointerPosition().y);
+    //   if(stage===stageMagnitude){
+    //     $.ajax({
     //     type: "POST",
     //     url: "/data/1",
     //     data: JSON.stringify({values}),
     //     contentType: "application/json",
     //     dataType: 'json'
-    //   });
-    // }else{
+    //   });}else{
     //   $.ajax({
     //     type: "POST",
     //     url: "/data/2",
@@ -31,26 +114,37 @@ function getIndex(cropped, image) {
     //     dataType: 'json'
     //   });
     // }
+  }
+  stage.add(layer);
+}
+function drawImage(img,path,layer){
+  img.src = `${path}`;
+  img.onload = function() {
+  var theImg = new Konva.Image({
+    image: img,
+    x: 0,
+    y: 0,
+    width: 500,
+    height: 320,
   });
+  layer.add(theImg);
+  layer.draw();
 }
-function upload_image_action(image, button, icon) {
-  button.style.display = `none`;
-  icon.style.display = `none`;
 }
-magnitudeImageInput.addEventListener("change", function () {
+magnitudeImageInput.addEventListener("change", function () { 
   let reader = new FileReader();
   magnitudeImage.style.display = `flex`;
-  reader.addEventListener("load", () => {
-    path = reader.result;
-    magnitudeImage.src = `${path}`;
-    cropper1 = new Cropper(magnitudeImage, {});
-    getIndex(cropper1, magnitudeImage);
-    upload_image_action(magnitudeImage, magnitudeImageBtn, mag_icon);
+  reader.addEventListener("load" ,() => {
+  path = reader.result;
+  stageMagnitude=drawStage('canvas-magnitude');
+  layerMagnitude=drawLayer(stageMagnitude);
+  drawImage(magnitudeImage,path,layerMagnitude,stageMagnitude);
+  drawRect(stageMagnitude,layerMagnitude,magnitudeImage);
+  upload_image_action(magnitudeImage,magnitudeImageBtn);
   });
   reader.readAsDataURL(this.files[0]);
-  let formData = new FormData();
-  formData.append("file", this.files[0]);
-  console.log(formData);
+  // let formData = new FormData();
+  // formData.append("file", this.files[0]);
   //   $.ajax({
   //     type: "POST",
   //     url: "/image/1",
@@ -66,40 +160,26 @@ magnitudeImageInput.addEventListener("change", function () {
 phaseImageInput.addEventListener("change", function () {
   let reader = new FileReader();
   phaseImage.style.display = `flex`;
-  reader.addEventListener("load", () => {
-    path = reader.result;
-    path = reader.result;
-    phaseImage.src = `${path}`;
-    cropper2 = new Cropper(phaseImage, {});
-    getIndex(cropper2, phaseImage);
-    upload_image_action(phaseImage, phaseImageBtn, phase_icon);
+  reader.addEventListener("load" ,() => {
+  path = reader.result; 
+  stagePhase=drawStage('canvas-phase');
+  layerPhase=drawLayer(stagePhase);
+  drawImage(phaseImage,path,layerPhase,stagePhase);
+  drawRect(stagePhase,layerPhase,phaseImage);
+  upload_image_action(phaseImage,phaseImageBtn);
   });
-  reader.readAsDataURL(this.files[0]);
-  let formData = new FormData();
-  formData.append("file", this.files[0]);
-  console.log(formData);
+reader.readAsDataURL(this.files[0]);
+let formData = new FormData();
+  // formData.append("file", this.files[0]);
   // $.ajax({
-  //   type: "POST",
-  //   url: "/image/2",
-  //   data: formData,
-  //   contentType: false,
-  //   cache: false,
-  //   processData: false,
-  //   async: true,
-  //   success: function () {
-  //   },
+  //     type: "POST",
+  //     url: "/image/2",
+  //     data: formData,
+  //     contentType: false,
+  //     cache: false,
+  //     processData: false,
+  //     async: true,
+  //     success: function () {
+  //     },
   // });
 });
-// function  resultImage( returned_data){
-
-//     returned_data = JSON.parse(returned_data)
-//     if (returned_data['success'] > 0){ // generation is done
-//         const byteCharacters = atob(returned_data['image']);
-//         const byteNumbers = new Array(byteCharacters.length);
-//         for (let i = 0; i < byteCharacters.length; i++) {
-//             byteNumbers[i] = byteCharacters.charCodeAt(i);
-//         }
-//         const byteArray = new Uint8Array(byteNumbers);
-//         document.getElementById("phaseImage-result").src = URL.createObjectURL(new Blob([byteArray], { type: 'image/png' }));
-//     }
-// }
