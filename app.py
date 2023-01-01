908cb521a223a47c6c7fcd1a3149863b9a2d0fac
Mohamed Salah <85584384+Ms850446@users.noreply.github.com>
2023-01-01 07:15:24 +0200

    done connections

--- app.py
+++ app.py
@@ -7,64 +7,86 @@ from PIL import Image
 from IMAGE import ImageClass
 from PROCESSING import ProcessingClass
 
-UPLOAD_FOLDER = './upload'
+UPLOAD_FOLDER = 'static/imgs'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__, template_folder="templates")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 values = {"magnitude": {"x1": None, "y1": None, "x2": None, "y2": None},
           "phase": {"x1": None, "y1": None, "x2": None, "y2": None}}
+filter = None  # >> 0 for low pass filter and 1 for high pass filter
+shape = None  # >> 0 for rectangle and 1  for the circle
+changeOutput = False
 imagePath = {"magnitude": "", "phase": "", "combined": ""}
 
 # Main Function(connection the functions together):
-def Main(img1, img2, x1_amp, x2_amp, y1_amp, y2_amp, x1_phase, x2_phase, y1_phase, y2_phase, cut_flag, filter_flag):  
+
+
+def Main(img1, img2, x1_amp, x2_amp, y1_amp, y2_amp, x1_phase, x2_phase, y1_phase, y2_phase, cut_flag, filter_flag):
+    print("welcome from the main function here is the images")
     combined_image = 0
     img1_path = ImageClass(path=img1)  # First Object
     img2_path = ImageClass(path=img2)  # Second Object
-    image1 = img1_path.read()  
-    image2 = img2_path.read()  
-    
+    image1 = img1_path.read()
+    image2 = img2_path.read()
+
     img1_gray = ImageClass.grayScale(image1)
     img2_gray = ImageClass.grayScale(image2)
-    
+
     image1_resized = ProcessingClass.Resize(img1_gray)
     image2_resized = ProcessingClass.Resize(img2_gray)
-    
+
     image1_resized_fft = ImageClass.fourierTransform(image1_resized)
     image2_resized_fft = ImageClass.fourierTransform(image2_resized)
-    
-    image1_amplitude, image1_phase = ImageClass.separateMagnitudePhase(image1_resized_fft)
-    image2_amplitude, image2_phase = ImageClass.separateMagnitudePhase(image2_resized_fft)
 
-    
+    image1_amplitude, image1_phase = ImageClass.separateMagnitudePhase(
+        image1_resized_fft)
+    image2_amplitude, image2_phase = ImageClass.separateMagnitudePhase(
+        image2_resized_fft)
+
     if cut_flag == 0:  # Cut in a rectangle shape
         if filter_flag == 0:  # Low Pass Filter
-            cutted_phase_img = ProcessingClass.crop_2d_img_rect(image2_phase, x1_phase, x2_phase, y1_phase, y2_phase)
-            cutted_amplitude_img = ProcessingClass.crop_2d_img_rect(image1_amplitude, x1_amp, x2_amp, y1_amp, y2_amp)
-            
+            cutted_phase_img = ProcessingClass.crop_2d_img_rect(
+                image2_phase, x1_phase, x2_phase, y1_phase, y2_phase)
+            cutted_amplitude_img = ProcessingClass.crop_2d_img_rect(
+                image1_amplitude, x1_amp, x2_amp, y1_amp, y2_amp)
+
         if filter_flag == 1:  # High Pass Filter
-            cutted_phase_img = ProcessingClass.highPassFilterRect(image2_phase, x1_phase, x2_phase, y1_phase, y2_phase)
-            cutted_amplitude_img = ProcessingClass.highPassFilterRect(image1_amplitude, x1_amp, x2_amp, y1_amp, y2_amp)
-        
+            cutted_phase_img = ProcessingClass.highPassFilterRect(
+                image2_phase, x1_phase, x2_phase, y1_phase, y2_phase)
+            cutted_amplitude_img = ProcessingClass.highPassFilterRect(
+                image1_amplitude, x1_amp, x2_amp, y1_amp, y2_amp)
+
     if cut_flag == 1:  # Cut in a circle shape
-        radius_phase = ProcessingClass.distance_between_two_points(x1_phase, x2_phase, y1_phase, y2_phase)
-        radius_magnitude = ProcessingClass.distance_between_two_points(x1_amp, x2_amp, y1_amp, y2_amp)
+        radius_phase = ProcessingClass.distance_between_two_points(
+            x1_phase, x2_phase, y1_phase, y2_phase)
+        radius_magnitude = ProcessingClass.distance_between_two_points(
+            x1_amp, x2_amp, y1_amp, y2_amp)
         if filter_flag == 0:  # Low Pass Filter
-            cutted_phase_img = ProcessingClass.crop_2d_img_cir(image2_phase, x1_phase, y1_phase, radius_phase)
-            cutted_amplitude_img = ProcessingClass.crop_2d_img_cir(image1_amplitude, x1_amp, y1_amp, radius_magnitude)
-        
+            cutted_phase_img = ProcessingClass.crop_2d_img_cir(
+                image2_phase, x1_phase, y1_phase, radius_phase)
+            cutted_amplitude_img = ProcessingClass.crop_2d_img_cir(
+                image1_amplitude, x1_amp, y1_amp, radius_magnitude)
+
         if filter_flag == 1:  # High Pass Filter
-            cutted_phase_img = ProcessingClass.highPassFilterCir(image2_phase, x1_phase, y1_phase, radius_phase)
-            cutted_amplitude_img = ProcessingClass.highPassFilterCir(image1_amplitude, x1_amp, y1_amp, radius_magnitude)
-           
-    combined_image = ProcessingClass.combination(cutted_amplitude_img, cutted_phase_img)
+            cutted_phase_img = ProcessingClass.highPassFilterCir(
+                image2_phase, x1_phase, y1_phase, radius_phase)
+            cutted_amplitude_img = ProcessingClass.highPassFilterCir(
+                image1_amplitude, x1_amp, y1_amp, radius_magnitude)
+
+    combined_image = ProcessingClass.combination(
+        cutted_amplitude_img, cutted_phase_img)
 
     return combined_image
 
 
-def save_image(file, type):
+def get_image_extension(name):
+    return '.'+name.split('.')[-1]
+
+
+def save_image(file, type):  # the type is choose between magnitude or phase
     name = type+'.'+file.filename.split('.')[-1]
-    imagePath[type] = 'upload/'+name
+    imagePath[type] = 'static/imgs/'+name
     file_path_mag = os.path.join(app.config['UPLOAD_FOLDER'], name)
     file.save(file_path_mag)
 
@@ -87,15 +109,22 @@ def set_values(key, list):
 def index():
     magnitude = values['magnitude']
     phase = values["phase"]
-    if (magnitude['x1'] != None) and (phase["x1"] != None):
-        combinedImage = Functions.Main(imagePath["magnitude"], imagePath["phase"], magnitude["x1"], magnitude["x2"],
-                                       magnitude["y1"], magnitude["y2"], phase["x1"], phase["x2"], phase["y1"], phase["y2"])
+    if (magnitude['y2'] != None) and (phase["y2"] != None):
+        combinedImage = Main(imagePath["magnitude"], imagePath["phase"], magnitude["x1"], magnitude["x2"],
+                             magnitude["y1"], magnitude["y2"], phase["x1"], phase["x2"], phase["y1"], phase["y2"], shape, filter)
         # print("here")
-        # im=cv2.imshow("image",combinedImage)
+        # im=cv2.imread(imagePath["magnitude"])/////
+        # im = cv2.imshow("image", imagePath["magnitude"])
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
         # im = Image.fromarray(combinedImage)
-        # im.save("upload/combined.jpg")
+        # cv2.imwrite("static/imgs/combined.png",im)/////
+        # get_image_extension(imagePath["magnitude"]),im)
+        # global changeOutput/////
+        # if changeOutput:
+        #     result={"path":"static/imgs/combined.png","valid":True}
+        #     return jsonify(result)
+        #     changeOutput=False
     return render_template('main.html')
 
 
@@ -115,6 +144,7 @@ def image(id):
 
 @app.route('/data/<int:id>', methods=['POST'])
 def data(id):
+    print("here is the function ")
     if request.method == 'POST':
         key = ""
         form = request.get_json()
@@ -125,7 +155,12 @@ def data(id):
         else:
             key = "phase"
         print(form["values"])
+        # 0 for rectangle and 1 for circle for index 4 and for index 5 >> 0 for low pass filter and 1 for high pass filter
+        shape = form["values"][4]
+        filter = form["values"][5]
         set_values(key, form["values"])
+    global changeOutput
+    changeOutput = True
     return redirect('/')
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -8,8 +8,8 @@ let mag_icon = document.querySelector("#mag-icon");
 let phase_icon = document.querySelector("#phase-icon");
 let iamge;
 let cir;
-let filterFlag=0;
-let shapeFlag=0;
+let filterFlag = 0;
+let shapeFlag = 0;
 let rect;
 var path = "";
 let rectMag;
@@ -17,46 +17,56 @@ let rectPhase;
 let cirMag;
 let cirPhase;
 let rectFlag = 1;
-let circleFlag=0;
+let circleFlag = 0;
 let magStage;
 let phaseStage;
 let rectArray1 = [];
 let rectArray2 = [];
-let cirArray1=[];
-let cirArray2=[];
-let stagArray=[];
+let cirArray1 = [];
+let cirArray2 = [];
+let stagArray = [];
 let isNowDrawing = false;
 var path = "";
-function send(id,values){
-  console.log(id)
-      $.ajax({
-        type: "POST",
-        url: "/data/"+id,
-        data: JSON.stringify({values}),
-        contentType: "application/json",
-        dataType: 'json'
-      });
+function send(id, values) {
+  console.log(id);
+  $.ajax({
+    type: "POST",
+    url: "/data/" + id,
+    data: JSON.stringify({ values }),
+    contentType: "application/json",
+    dataType: "json",
+    success: function (response) {
+      if (response["valid"] == true) {
+        let image = document.querySelector(".resultImage");
+        console.log("path is ");
+        console.log(response["path"]);
+        image.src = response["path"];
+      } else {
+        console.log("no data yet");
+      }
+    },
+  });
 }
-function upload_image_action(image,button) {
+function upload_image_action(image, button) {
   image.style.display = `flex`;
   button.style.display = `none`;
 }
-function drawStage(contain){
-containerUsed=contain;
-var stage = new Konva.Stage({
-  container: contain,
-  width: 500,
-  height: 300
-});
-return stage;
+function drawStage(contain) {
+  containerUsed = contain;
+  var stage = new Konva.Stage({
+    container: contain,
+    width: 500,
+    height: 300,
+  });
+  return stage;
 }
-function drawLayer(stage){
-var layer = new Konva.Layer();
-stage.add(layer);
-stage.draw();
-return layer;
+function drawLayer(stage) {
+  var layer = new Konva.Layer();
+  stage.add(layer);
+  stage.draw();
+  return layer;
 }
-function circleDown(stage,layer) {
+function circleDown(stage, layer) {
   cir = new Konva.Circle({
     x: stage.getPointerPosition().x,
     y: stage.getPointerPosition().y,
@@ -65,12 +75,12 @@ function circleDown(stage,layer) {
     stroke: "##1d27b6",
     strokeWidth: 2,
   });
-  if(stage===magStage){
-    cirMag=cir;
+  if (stage === magStage) {
+    cirMag = cir;
     layer.add(cirMag);
     cirArray1.push(cirMag);
-  }else{
-    cirPhase=cir;
+  } else {
+    cirPhase = cir;
     layer.add(cirPhase);
     cirArray2.push(cirPhase);
   }
@@ -96,7 +106,7 @@ function circleMove(stage) {
   const newRadius = Math.sqrt(rise + run);
   cir.radius(newRadius);
 }
-function rectDown(stage,layer){
+function rectDown(stage, layer) {
   rect = new Konva.Rect({
     x: stage.getPointerPosition().x,
     y: stage.getPointerPosition().y,
@@ -104,14 +114,14 @@ function rectDown(stage,layer){
     height: 0,
     fill: "transparent",
     stroke: "#1d27b6",
-    strokeWidth: 2
+    strokeWidth: 2,
   });
-  if(stage===magStage){
-    rectMag=rect;
+  if (stage === magStage) {
+    rectMag = rect;
     layer.add(rectMag);
     rectArray1.push(rectMag);
-  }else{
-    rectPhase=rect;
+  } else {
+    rectPhase = rect;
     layer.add(rectPhase);
     rectArray2.push(rectPhase);
   }
@@ -129,191 +139,197 @@ function rectDown(stage,layer){
   // });
   // layer.add(tr1);
   layer.draw();
-  
 }
-function rectMove(stage){
+function rectMove(stage) {
   const newWidth = stage.getPointerPosition().x - rect.x();
   const newHeight = stage.getPointerPosition().y - rect.y();
   rect.width(newWidth);
   rect.height(newHeight);
 }
-function drawRect(stage,layer){
-  stage.on("mousedown ",(e)=> mousedownHandler(e));
+function drawRect(stage, layer) {
+  stage.on("mousedown ", (e) => mousedownHandler(e));
   stage.on("mousemove ", mousemoveHandler);
   stage.on("mouseup ", mouseupHandler);
+  let valuesMag = [];
+  let valuesPhase = [];
   function mousedownHandler() {
-  //   if(containerUsed==="canvas-magnitude"){
-  //   if(rectArray1.length>0 ){
-  //     isNowDrawing=false;
-  //     values=[];
-  //   }else{
-  //     isNowDrawing = true;
-  //     if(circleFlag===1){
-  //     circleDown(stage,layer);
-  //     }else{
-  //     rectDown(stage,layer);  
-  //     }  
-  //   }
-  // }else{
-  //   if(rectArray2.length>0){
-  //     isNowDrawing=false;
-  //     values=[];
-  //   }else{
-  //     isNowDrawing = true;
-  //     if(circleFlag===1){
-  //     circleDown(stage,layer);
-  //     }else{
-  //     rectDown(stage,layer);  
-  //     }  
-  //   }
-  // }
-  let valuesMag=[];
-  let valuesPhase=[];
-  if(stage===magStage){
-    if(rectArray1.length>0){
-      rectMag.destroy();
-      valuesMag=[];
-    }
-    if(cirArray1.length>0){
-        cirMag.destroy();
-        valuesMag=[];
-    }
-    valuesMag.push(stage.getPointerPosition().x);
-    valuesMag.push(stage.getPointerPosition().y);
-  }else{
-      if(rectArray2.length>0){
-        rectPhase.destroy();
-        valuesPhase=[];
+    //   if(containerUsed==="canvas-magnitude"){
+    //   if(rectArray1.length>0 ){
+    //     isNowDrawing=false;
+    //     values=[];
+    //   }else{
+    //     isNowDrawing = true;
+    //     if(circleFlag===1){
+    //     circleDown(stage,layer);
+    //     }else{
+    //     rectDown(stage,layer);
+    //     }
+    //   }
+    // }else{
+    //   if(rectArray2.length>0){
+    //     isNowDrawing=false;
+    //     values=[];
+    //   }else{
+    //     isNowDrawing = true;
+    //     if(circleFlag===1){
+    //     circleDown(stage,layer);
+    //     }else{
+    //     rectDown(stage,layer);
+    //     }
+    //   }
+    // }
+    if (stage === magStage) {
+      if (rectArray1.length > 0) {
+        rectMag.destroy();
+        valuesMag = [];
       }
-      if(cirArray2.length>0){
-          cirPhase.destroy();
-          valuesPhase=[];
+      if (cirArray1.length > 0) {
+        cirMag.destroy();
+        valuesMag = [];
+      }
+      valuesMag.push(stage.getPointerPosition().x);
+      valuesMag.push(stage.getPointerPosition().y);
+    } else {
+      if (rectArray2.length > 0) {
+        rectPhase.destroy();
+        valuesPhase = [];
+      }
+      if (cirArray2.length > 0) {
+        cirPhase.destroy();
+        valuesPhase = [];
       }
       valuesPhase.push(stage.getPointerPosition().x);
       valuesPhase.push(stage.getPointerPosition().y);
     }
-      isNowDrawing = true;
-      if(circleFlag===1){
-      circleDown(stage,layer);
-      }else{
-      rectDown(stage,layer);
-      } 
+    isNowDrawing = true;
+    if (circleFlag === 1) {
+      circleDown(stage, layer);
+    } else {
+      rectDown(stage, layer);
+    }
   }
   function mousemoveHandler() {
-      if (!isNowDrawing) return false;
-      if(circleFlag===1){
-        circleMove(stage);
-      }else{
-        rectMove(stage);
-      }
+    if (!isNowDrawing) return false;
+    if (circleFlag === 1) {
+      circleMove(stage);
+    } else {
+      rectMove(stage);
+    }
   }
   function mouseupHandler() {
-      isNowDrawing = false;
-      if(stage===magStage){
-        valuesMag.push(stage.getPointerPosition().x);
-        valuesMag.push(stage.getPointerPosition().y);
-        valuesMag.push(shapeFlag);
-        valuesMag.push(filterFlag)
-        send(1,valuesMag);
-        console.log(valuesMag);
-      }else{
-        valuesPhase.push(stage.getPointerPosition().x);
-        valuesPhase.push(stage.getPointerPosition().y);
-        valuesPhase.push(shapeFlag);
-        valuesPhase.push(filterFlag);
-        send(2,valuesPhase);
-        console.log(valuesPhase);
+    isNowDrawing = false;
+    if (stage === magStage) {
+      valuesMag.push(stage.getPointerPosition().x);
+      valuesMag.push(stage.getPointerPosition().y);
+      valuesMag.push(shapeFlag);
+      valuesMag.push(filterFlag);
+      send(1, valuesMag);
+      console.log(valuesMag);
+    } else {
+      valuesPhase.push(stage.getPointerPosition().x);
+      valuesPhase.push(stage.getPointerPosition().y);
+      valuesPhase.push(shapeFlag);
+      valuesPhase.push(filterFlag);
+      send(2, valuesPhase);
+      console.log(valuesPhase);
     }
   }
   stage.add(layer);
 }
-function drawImage(img,path,layer){
+function drawImage(img, path, layer) {
   img.src = `${path}`;
-  img.onload = function() {
-  theImg = new Konva.Image({
-    image: img,
-    x: 0,
-    y: 0,
-    width: 500,
-    height: 300,
-  });
-  layer.add(theImg);
-  layer.draw();
+  img.onload = function () {
+    theImg = new Konva.Image({
+      image: img,
+      x: 0,
+      y: 0,
+      width: 500,
+      height: 300,
+    });
+    layer.add(theImg);
+    layer.draw();
+  };
 }
-}
-magnitudeImageInput.addEventListener("change",() => { 
-  upload(magnitudeImage,1,"canvas-magnitude",magnitudeImageBtn,magnitudeImageInput)
-}
+magnitudeImageInput.addEventListener("change", () => {
+  upload(
+    magnitudeImage,
+    1,
+    "canvas-magnitude",
+    magnitudeImageBtn,
+    magnitudeImageInput
   );
-phaseImageInput.addEventListener("change",()=> {
-upload(phaseImage,2,"canvas-phase",phaseImageBtn,phaseImageInput)
-}
-);
-function upload(uploadImage,number,container,uploadButton,input){
+});
+phaseImageInput.addEventListener("change", () => {
+  upload(phaseImage, 2, "canvas-phase", phaseImageBtn, phaseImageInput);
+});
+function upload(uploadImage, number, container, uploadButton, input) {
   reader = new FileReader();
   uploadImage.style.display = `flex`;
-  reader.addEventListener("load" ,() => {
-  path = reader.result;
-  stage=drawStage(container); 
-  if(container=="canvas-magnitude"){
-    magStage=stage;
-  }else{
-    phaseStage=stage;
-  }
-  stagArray.push(stage);
-  layer=drawLayer(stage);
-  image=drawImage(uploadImage,path,layer,stage);
-  drawRect(stage,layer,uploadImage);
-  upload_image_action(uploadImage,uploadButton);
+  reader.addEventListener("load", () => {
+    path = reader.result;
+    stage = drawStage(container);
+    if (container == "canvas-magnitude") {
+      magStage = stage;
+    } else {
+      phaseStage = stage;
+    }
+    stagArray.push(stage);
+    layer = drawLayer(stage);
+    image = drawImage(uploadImage, path, layer, stage);
+    drawRect(stage, layer, uploadImage);
+    upload_image_action(uploadImage, uploadButton);
   });
   reader.readAsDataURL(input.files[0]);
-  
+
   let formData = new FormData();
   formData.append("file", input.files[0]);
-    $.ajax({
-      type: "POST",
-      url: "/image/"+number,
-      data: formData,
-      contentType: false,
-      cache: false,
-      processData: false,
-      async: true,
-      success: function () {
-      },
+  $.ajax({
+    type: "POST",
+    url: "/image/" + number,
+    data: formData,
+    contentType: false,
+    cache: false,
+    processData: false,
+    async: true,
+    success: function () {},
   });
 }
-function deleteData(){
-  for(i=0;i<stagArray.length;i++){
-  stagArray[i].destroy();  
+function deleteData() {
+  for (i = 0; i < stagArray.length; i++) {
+    stagArray[i].destroy();
   }
   magnitudeImageBtn.style.display = `flex`;
-  phaseImageBtn.style.display=`flex`;
-  magnitudeImageInput.addEventListener("change",() => { 
-    upload(magnitudeImage,1,"canvas-magnitude",magnitudeImageBtn,magnitudeImageInput)
-  }
+  phaseImageBtn.style.display = `flex`;
+  magnitudeImageInput.addEventListener("change", () => {
+    upload(
+      magnitudeImage,
+      1,
+      "canvas-magnitude",
+      magnitudeImageBtn,
+      magnitudeImageInput
     );
-  phaseImageInput.addEventListener("change",()=> {
-  upload(phaseImage,2,"canvas-phase",phaseImageBtn,phaseImageInput)
-  }
-  );
+  });
+  phaseImageInput.addEventListener("change", () => {
+    upload(phaseImage, 2, "canvas-phase", phaseImageBtn, phaseImageInput);
+  });
 }
-function dataCircle(){
-  circleFlag=1;
-  rectFlag=0;
-  shapeFlag=1;
+function dataCircle() {
+  circleFlag = 1;
+  rectFlag = 0;
+  shapeFlag = 1;
   rectMag.destroy();
   rectPhase.destroy();
 }
-function dataRect(){
-  circleFlag=0;
-  rectFlag=1;
-  shapeFlag=0;
+function dataRect() {
+  circleFlag = 0;
+  rectFlag = 1;
+  shapeFlag = 0;
   cirMag.destroy();
   cirPhase.destroy();
 }
-function highFilter(){
-  filterFlag=1;
+function highFilter() {
+  filterFlag = 1;
 }
-function lowFilter(){
-  filterFlag=0;
+function lowFilter() {
+  filterFlag = 0;
 }
